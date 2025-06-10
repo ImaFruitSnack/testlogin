29512a7c42bdb98f7e83360ee3bf775d08413fca
ImaFruitSnack <4yrmaddict@gmail.com>
2025-06-10 16:14:23 -0500

    Add Files

--- myfirst1.js
+++ myfirst1.js
@@ -24,7 +24,7 @@ async function run() {
     const query = { user: 'Fruit' };
     const user = await users.findOne(query);
     console.log(user);
-	global.mtest = user;
+	global.mtest = user.find(name);
 	return mtest
   } finally {
     await client.close();
