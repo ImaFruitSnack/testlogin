845edb1cb913ad2efe01ec5d4f286ad30928e72a
ImaFruitSnack <4yrmaddict@gmail.com>
2025-06-09 21:02:11 -0500

    Add files

--- myfirst1.js
+++ myfirst1.js
@@ -20,6 +20,19 @@ const client = new MongoClient(uri, {
   }
 });
 
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+
 
 application.get(`/`, async(req, res) => {
 	res.sendFile(path.join(path.join(__dirname, `public`), `webfile.html`))
@@ -30,12 +43,14 @@ application.get(`/`, async(req, res) => {
 	const txt = q.year + " " + q.month;
 	let data = await fs.readFileSync('./public/webfile.html');
 	res.write(data);
+	run().catch(console.dir);
 	res.end(txt);
 })
 
 application.get('/subserver', async(req,res) => {
 	res.sendFile(path.join(path.join(__dirname, `public`), `webfile2.html`))
 })
+
 
 let server = http.createServer(application)
 server.listen(8080, `0.0.0.0`)
